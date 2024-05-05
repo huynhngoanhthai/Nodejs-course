ad8e457be097692109dc0da3d199aa5bff83b6f9
Huỳnh Ngô Anh Thái <74443669+huynhngoanhthai@users.noreply.github.com>
2024-05-05 10:39:37 +0700

    update

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("Hello world")
+[1, 2].forEach((value) => console.log(value))
+
+let language = "JavaScript";
+let message = "Hello";
+let date = "Monday";
+/**
+ * The function `caCon` appends the string "meo" to the input string and returns the result.
+ * @param speak - The `caCon` function takes a parameter called `speak`, which is a string representing
+ * a sound or a word. The function appends the string "meo" to the input string and returns the
+ * concatenated result.
+ * @returns The function `caCon` is returning the input `speak` concatenated with the string "meo".
+ */
+function caCon (speak){
+    return speak + "meo";
+}
